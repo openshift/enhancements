3b8caa055c69f7eed0af479c33844d88151b63d9
Doug Hellmann <dhellmann@redhat.com>
2021-02-05 18:11:07 -0500

    tools: wrap long lines to appease the linter
    Go back to breaking long lines up, but only split anything long enough
    for the linter to complain instead of trying to wrap everything.
    
    Signed-off-by: Doug Hellmann <dhellmann@redhat.com>

--- tools/report/report_test.go
+++ tools/report/report_test.go
@@ -6,7 +6,10 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+const longLine string = "The OVN-Kubernetes network type uses [OVN](https://www.ovn.org) to implement node overlay networks for Kubernetes. When OVN-Kubernetes is used as the network type for an Openshift cluster, OVN ACLs are the used to implement Kubernetes  network policies ('NetworkPolicy' resources).  ACL's can either allow or deny traffic by matching on packets with specific rules. Built into the OVN ACL feature is"
+
 func TestFormatDescription(t *testing.T) {
+
 	for _, tc := range []struct {
 		Scenario string
 		Input    string
@@ -59,9 +62,88 @@ Fourth line
   > Third line
   > Fourth line`,
 		},
+		{
+			Scenario: "Long line",
+			Input:    longLine,
+			Expected: `  > The OVN-Kubernetes network type uses [OVN](https://www.ovn.org) to implement node overlay networks for Kubernetes. When OVN-Kubernetes is used as the network type for an Openshift cluster, OVN ACLs are the used to implement Kubernetes  network policies ('NetworkPolicy' resources).  ACL's can either allow or deny traffic by matching on packets with specific rules. Built into the OVN ACL feature
+  > is`,
+		},
 	} {
 		t.Run(tc.Scenario, func(t *testing.T) {
 			actual := formatDescription(tc.Input, descriptionIndent)
+			assert.Equal(t, tc.Expected, actual)
+		})
+	}
+}
+
+func TestFindSplit(t *testing.T) {
+	for _, tc := range []struct {
+		Scenario string
+		Input    string
+		Start    int
+		Expected int
+	}{
+		{
+			Scenario: "no spaces",
+			Input:    "this-string-has-no-spaces",
+			Expected: 0,
+		},
+		{
+			Scenario: "one space",
+			Input:    "one space",
+			Expected: 3,
+		},
+		{
+			Scenario: "multiple spaces",
+			Input:    "this string has no spaces",
+			Expected: 18,
+		},
+		{
+			Scenario: "past end",
+			Input:    "short",
+			Start:    100,
+			Expected: 0,
+		},
+	} {
+		t.Run(tc.Scenario, func(t *testing.T) {
+			start := tc.Start
+			if start == 0 {
+				start = len(tc.Input) - 1
+			}
+			actual := findSplit(tc.Input, start)
+			assert.Equal(t, tc.Expected, actual)
+		})
+	}
+}
+
+func TestWrapLine(t *testing.T) {
+	for _, tc := range []struct {
+		Scenario string
+		Input    string
+		Len      int
+		Expected []string
+	}{
+		{
+			Scenario: "no spaces",
+			Input:    "this-string-has-no-spaces",
+			Len:      5,
+			Expected: []string{"this-string-has-no-spaces"},
+		},
+		{
+			Scenario: "one space",
+			Input:    "one space",
+			Len:      5,
+			Expected: []string{"one", "space"},
+		},
+		{
+			Scenario: "long line",
+			Input:    longLine,
+			Len:      maxLineLength - 4,
+			Expected: []string{"The OVN-Kubernetes network type uses [OVN](https://www.ovn.org) to implement node overlay networks for Kubernetes. When OVN-Kubernetes is used as the network type for an Openshift cluster, OVN ACLs are the used to implement Kubernetes  network policies ('NetworkPolicy' resources).  ACL's can either allow or deny traffic by matching on packets with specific rules. Built into the OVN ACL feature", "is"},
+		},
+	} {
+		t.Run(tc.Scenario, func(t *testing.T) {
+			actual := wrapLine(tc.Input, tc.Len)
 			assert.Equal(t, tc.Expected, actual)
 		})
 	}
