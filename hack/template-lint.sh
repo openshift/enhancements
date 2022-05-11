4585f20f7b0a25278f857ec1646a6a0904f3dd0c
Doug Hellmann <dhellmann@redhat.com>
2022-05-11 10:29:50 -0400

    lint: only apply template lint rules to new files
    Ignore existing files during template lint. We don't expect authors to
    update to use the new template, so this change ignores existing files
    so we don't have to manually override the job failure each time.
    
    Signed-off-by: Doug Hellmann <dhellmann@redhat.com>

--- hack/template-lint.sh
+++ hack/template-lint.sh
@@ -14,7 +14,12 @@ TEMPLATE=${SCRIPTDIR}/../guidelines/enhancement_template.md
 # We only look at the files that have changed in the current PR, to
 # avoid problems when the template is changed in a way that is
 # incompatible with existing documents.
-CHANGED_FILES=$(${SCRIPTDIR}/find_changed.sh)
+CHANGED_FILES=$(${SCRIPTDIR}/find_new.sh)
+
+if [ -z "$CHANGED_FILES" ]; then
+    echo "OK, no new enhancement files found"
+    exit 0
+fi
 
 RC=0
 for file in $CHANGED_FILES
