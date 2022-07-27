b72b6bef56a19588e93e11eaea5fcde56fcb8079
Doug Hellmann <dhellmann@redhat.com>
2022-07-27 16:39:41 -0400

    tools: pin markdown lint tool versions
    Updated versions of markdownlint are requiring newer versions of node. Pin
    everything to compatible older versions that do not add rules we would
    ignore.
    
    Signed-off-by: Doug Hellmann <dhellmann@redhat.com>

--- hack/install-markdownlint.sh
+++ hack/install-markdownlint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -xe
 
-dnf -y module enable nodejs:12
+dnf -y module enable nodejs:14
 dnf -y install nodejs
-npm install -g markdownlint markdownlint-cli2
+npm install -g markdownlint@v0.25.1 markdownlint-cli2@v0.4.0
